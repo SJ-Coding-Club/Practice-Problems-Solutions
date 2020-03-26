0338aa8eee329dd0d1724044ee710a27fc1d6aad
Jack Donofrio <53123020+Jack1q@users.noreply.github.com>
2020-03-25 23:20:23 -0400

    Update and rename Python/Python-Chapter-7/seven_01.py to Python/Python-Chapter-7/Rectangle-Class/Rectangle.py

---
+++ Python/Python-Chapter-7/Rectangle-Class/Rectangle.py
@@ -0,0 +1,11 @@
+class Rectangle:
+    def __init__(self, width = 1, height = 2):
+        self.__width = width
+        self.__height = height
+    
+    def getArea(self):
+        return self.__width * self.__height
+    
+    def getPerimeter(self):
+        return 2 * (self.__width + self.__height)
+      
